4731685ef17112c47adcf9d0536d6417c2a60eed
shine.xgh <shine.xgh@gmail.com>
2021-02-19 11:25:30 +0800

    perceptron: add dual

---
+++ perceptron/README.md
@@ -0,0 +1,13 @@
+# Perceptron
+
+## p.py
+
+```bash
+$ python p.py --help
+usage: p.py [-h] [--alpha ALPHA] [--epoch EPOCH]
+
+optional arguments:
+  -h, --help     show this help message and exit
+  --alpha ALPHA  learning rate
+  --epoch EPOCH  epoch
+```
--- perceptron/p.py
+++ perceptron/p.py
@@ -24,24 +24,60 @@ def load_data():
 
 class Perceptron(object):
 
-    def __init__(self, input_dim=4, alpha=0.01):
+    def __init__(self, x_train, y_train, input_dim=4, alpha=0.01):
         self.input_dim = input_dim
-        self.w = np.random.uniform(size=(input_dim, ))
+        self.w = np.zeros((input_dim, ))
         self.b = 0
         self.alpha = alpha
+        self.x_train = x_train
+        self.y_train = y_train
 
-    def train(self, x, y):
+    def train(self, xi):
+        x, y = self.x_train[xi], self.y_train[xi]
         pred = y * (np.dot(x, self.w) + self.b)
         if pred <= 0:
             self.w += self.alpha * y * x
             self.b += self.alpha * y
 
     def pred(self, x):
+        logging.info('w: %s', self.w)
         return np.dot(x, self.w) + self.b
+
+
+class PerceptronDual(object):
+
+    def __init__(self, x_train, y_train, input_dim=4, alpha=0.01):
+        self.input_dim = input_dim
+        self.num_train = len(x_train)
+        self.a = np.zeros((self.num_train, ))
+        self.b = 0
+        self.alpha = alpha
+        self.gram = np.zeros((self.num_train, self.num_train))
+        self.x_train = x_train
+        self.y_train = y_train
+        for i, xi in enumerate(x_train):
+            for j, xj in enumerate(x_train):
+                self.gram[i][j] = np.dot(xi, xj) * y_train[i]
+
+    def train(self, xi):
+        y = self.y_train[xi]
+        s = self.b
+        for j in range(self.num_train):
+            s += self.gram[j][xi] * self.a[j]
+        pred = y * s
+        if pred <= 0:
+            self.a[xi] += self.alpha
+            self.b += self.alpha * y
+
+    def pred(self, x):
+        w = np.sum(self.a.reshape(-1, 1) * self.x_train * self.y_train.reshape(-1, 1), axis=0)
+        logging.info('w: %s', w)
+        return np.dot(x, w) + self.b
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', type=str, default='', help="")
     parser.add_argument('--alpha', type=float, default=0.01, help="learning rate")
     parser.add_argument('--epoch', type=int, default=10, help="epoch")
     args = parser.parse_args()
@@ -52,11 +88,11 @@ def main():
     for i, x in enumerate(x_train[:10]):
         print(i, x, y_train[i])
 
-    model = Perceptron(input_dim=len(x_train[0]), alpha=args.alpha)
+    clazz = PerceptronDual if args.mode == 'dual' else Perceptron
+    model = clazz(x_train, y_train, input_dim=len(x_train[0]), alpha=args.alpha)
     for epoch in range(args.epoch):
-        for i, x in enumerate(x_train):
-            y = y_train[i]
-            model.train(x, y)
+        for i in range(len(x_train)):
+            model.train(i)
 
         y_pred_v = model.pred(x_test)
         y_pred = np.sign(y_pred_v)
