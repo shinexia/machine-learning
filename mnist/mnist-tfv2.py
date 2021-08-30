6e6ef896f13d4e4beceed6626d1fe0396785a947
shine.xgh <shine.xgh@gmail.com>
2021-08-30 15:59:27 +0800

    add mnist pytorch version

--- mnist/mnist-tfv2.py
+++ mnist/mnist-tfv2.py
@@ -13,16 +13,15 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 x_train = x_train[..., tf.newaxis].astype("float32")
 x_test = x_test[..., tf.newaxis].astype("float32")
 
-train_ds = tf.data.Dataset.from_tensor_slices(
-    (x_train, y_train)).shuffle(10000).batch(32)
-
-test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+batch_size = 64
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(batch_size)
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(batch_size)
 
 
 class MyModel(Model):
     def __init__(self):
         super(MyModel, self).__init__()
-        self.conv1 = Conv2D(32, 3, activation='relu')
+        self.conv1 = Conv2D(batch_size, 3, activation='relu')
         self.flatten = Flatten()
         self.d1 = Dense(128, activation='relu')
         self.d2 = Dense(10)
@@ -75,7 +74,7 @@ def test_step(images, labels):
     test_accuracy(labels, predictions)
 
 
-EPOCHS = 5
+EPOCHS = 14
 
 for epoch in range(EPOCHS):
     start_time = time.time()
